2abe9c285a1a028e912ead026fd55a001b26103a
Gabriel Damiani <gabriel.damiani@smn.com.br>
2023-11-23 17:51:26 -0300

    bug fix: cadastro no banco de dados

--- painel_acesso.php
+++ painel_acesso.php
@@ -1,10 +1,8 @@
 <?php
     include 'conexao_login.php';
     session_start();
-    echo"alo";
     
     if(isset($_POST["nomeAluno"]) && isset($_POST["matricula"]) && isset($_POST["nota1"]) && isset($_POST["nota2"])) {
-        echo "coisa";
         $nomeAluno = $mysqli->real_escape_string($_POST["nomeAluno"]);
         $matricula = $mysqli->real_escape_string($_POST["matricula"]);
         $nota1 = $mysqli->real_escape_string($_POST["nota1"]);
@@ -16,7 +14,7 @@
             echo"deu merda 2";
             // Evitar injeção de SQL usando prepared statement
             $stmt = $mysqli->prepare("INSERT INTO alunos (nome, matricula, nota1, nota2) VALUES (?, ?, ?, ?)");
-            $stmt->bind_param("sddd", $nomeAluno, $matricula, $nota1, $nota2);
+            $stmt->bind_param("ssss", $nomeAluno, $matricula, $nota1, $nota2);
             
             // Executar a declaração
             if ($stmt->execute()) {
@@ -34,105 +32,105 @@
     }
     
 
-//    busca de aluno (teste)
-    $conn = new mysqli("localhost", "root", "", "login");
-    if(isset($_POST["nomeAluno"])) {
-        if(empty($_POST["nomeAluno"])) {
-            echo "Atenção: Insira o nome do aluno que deseja buscar!";
-        }
-    }else { 
-        $nomeAluno = $mysqli->real_escape_string($_POST["nomeAluno"]);
-        $sql_code = "SELECT * FROM alunos WHERE nome = '$nomeAluno'";
-        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->connect_error);
-        $quantidade = $sql_query->num_rows;
+//  //    busca de aluno (teste)
+    // $conn = new mysqli("localhost", "root", "", "login");
+    // if(isset($_POST["nomeAluno"])) {
+    //     if(empty($_POST["nomeAluno"])) {
+    //         echo "Atenção: Insira o nome do aluno que deseja buscar!";
+    //     }
+    // }else { 
+    //     $nomeAluno = $mysqli->real_escape_string($_POST["nomeAluno"]);
+    //     $sql_code = "SELECT * FROM alunos WHERE nome = '$nomeAluno'";
+    //     $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->connect_error);
+    //     $quantidade = $sql_query->num_rows;
 
-        if ($quantidade > 0) {
-            // Exibe as informações do aluno
-            while ($row = $result->fetch_assoc()) {
-                echo "Nome: " . $row["nome"] . "<br>";
-                echo "Matrícula: " . $row["matricula"] . "<br>";
-                echo "nota 1: " . $row["nota1"] . "<br>";
-                echo "nota 2: " . $row["nota2"] . "<br>";
+    //     if ($quantidade > 0) {
+    //         // Exibe as informações do aluno
+    //         while ($row = $result->fetch_assoc()) {
+    //             echo "Nome: " . $row["nome"] . "<br>";
+    //             echo "Matrícula: " . $row["matricula"] . "<br>";
+    //             echo "nota 1: " . $row["nota1"] . "<br>";
+    //             echo "nota 2: " . $row["nota2"] . "<br>";
 
-            }
-        }else{
-            echo "nenhum aluno encontrado";
+    //         }
+    //     }else{
+    //         echo "nenhum aluno encontrado";
 
-        }
-    }
+    //     }
+    // }
     $mysqli->close();
 
 ?>
 
     <!DOCTYPE html>
     <html lang="pt-br">
-        <head>
+    <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="estilo_tabela.css">
         <title>Cadastro de Aluno</title>
-        </head>
-        <body>
-            <h3>Bem vindo, <?php echo isset($_SESSION["name"]) ? $_SESSION["name"] : "Usuário"; ?></h3> <!-- parte bonitinha de boas vindas no topo -->
-            <h1>Cadastro de Aluno</h1>
-            <form action="" method="post" id="cadastroaluno">
-                <label for="nomeAluno">Nome:</label>
-                <input type="text" name="nomeAluno" id="nome"><br><br>
-                <label for="matricula">Matrícula:</label>
-                <input type="text" name="matricula" id="matricula"><br><br>
-                <label for="nota1">Nota 1:</label>
-                <input type="number" name="nota1" step="0.1" id="nota1"><br><br>
-                <label for="nota2">Nota 2:</label>
-                <input type="number" name="nota2" step="0.1" id="nota2"><br><br>
-                <button type="submit">Cadastrar</button>
-            </form>
-            <h1>Tabela de alunos cadastrados</h1> <!-- será gerada com JS -->
-            <h3>Buscar aluno</h3>
-            <form action="" method="post" id="busca">
-                <label for="nome">Nome:</label>
-                <input type="text" name="nome" placeholder="nome do aluno" required><br><br>
-                <button type="submit">Pesquisar</button>
-            </form>
-            <table id="Alunos">
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Matrícula</th>
-                        <th>Nota 1</th>
-                        <th>Nota 2</th>
-                    </tr>
-                </thead>
-                <tbody>
-                        <!-- As linhas da tabela serão adicionadas aqui dinamicamente com JavaScript -->
-                </tbody>
-            </table>
-            <script>
-                document.getElementById("cadastroaluno").addEventListener("submit", function(event) {
-                    event.preventDefault(); 
-                    // Exemplo: Adicionando uma linha à tabela com dados do formulário
-                    const nome = document.getElementById("nome").value;
-                    const matricula = document.getElementById("matricula").value;
-                    const nota1 = parseFloat(document.getElementById("nota1").value);
-                    const nota2 = parseFloat(document.getElementById("nota2").value);
+    </head>
+    <body>
+        <h3>Bem vindo, <?php echo isset($_SESSION["name"]) ? $_SESSION["name"] : "Usuário"; ?></h3> <!-- parte bonitinha de boas vindas no topo -->
+        <h1>Cadastro de Aluno</h1>
+        <form id="cadastroaluno" action="" method="post">
+            <label for="nomeAluno">Nome:</label>
+            <input type="text" name="nomeAluno" id="nome"><br><br>
+            <label for="matricula">Matrícula:</label>
+            <input type="text" name="matricula" id="matricula"><br><br>
+            <label for="nota1">Nota 1:</label>
+            <input type="number" name="nota1" step="0.1" id="nota1"><br><br>
+            <label for="nota2">Nota 2:</label>
+            <input type="number" name="nota2" step="0.1" id="nota2"><br><br>
+            <button type="submit">Cadastrar</button>
+        </form>
+        <h1>Tabela de alunos cadastrados</h1> <!-- será gerada com JS -->
+        <h3>Buscar aluno</h3>
+        <form action="" method="post" id="busca">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" placeholder="nome do aluno" required><br><br>
+            <button type="submit">Pesquisar</button>
+        </form>
+        <table id="Alunos">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Matrícula</th>
+                    <th>Nota 1</th>
+                    <th>Nota 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                    <!-- As linhas da tabela serão adicionadas aqui dinamicamente com JavaScript -->
+            </tbody>
+        </table>
+        <script>
+            document.getElementById("cadastroaluno").addEventListener("submit", function(event) {
+                // event.preventDefault(); 
+                // Exemplo: Adicionando uma linha à tabela com dados do formulário
+                const nome = document.getElementById("nome").value;
+                const matricula = document.getElementById("matricula").value;
+                const nota1 = parseFloat(document.getElementById("nota1").value);
+                const nota2 = parseFloat(document.getElementById("nota2").value);
 
-                    const tabela = document.getElementById("Alunos");
-                    const linha = tabela.insertRow();
-                    const colunaNome = linha.insertCell(0);
-                    const colunaMatricula = linha.insertCell(1);
-                    const colunaNota1 = linha.insertCell(2);
-                    const colunaNota2 = linha.insertCell(3);
+                const tabela = document.getElementById("Alunos");
+                const linha = tabela.insertRow();
+                const colunaNome = linha.insertCell(0);
+                const colunaMatricula = linha.insertCell(1);
+                const colunaNota1 = linha.insertCell(2);
+                const colunaNota2 = linha.insertCell(3);
 
-                    colunaNome.innerHTML = nome;
-                    colunaMatricula.innerHTML = matricula;
-                    colunaNota1.innerHTML = nota1;
-                    colunaNota2.innerHTML = nota2;
+                colunaNome.innerHTML = nome;
+                colunaMatricula.innerHTML = matricula;
+                colunaNota1.innerHTML = nota1;
+                colunaNota2.innerHTML = nota2;
 
-                    document.getElementById('nome').value = '';
-                    document.getElementById('matricula').value = '';
-                    document.getElementById('nota1').value = '';
-                    document.getElementById('nota2').value = '';
-                });
-            </script>
-        </body>
+                // document.getElementById('nome').value = '';
+                // document.getElementById('matricula').value = '';
+                // document.getElementById('nota1').value = '';
+                // document.getElementById('nota2').value = '';
+            });
+        </script>
+    </body>
     </html>
