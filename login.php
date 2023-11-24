239fff7c3f09eba3736c2c9ec7372b325d575773
Camilly <Camilly.p.souza@gmail.com>
2023-11-23 22:11:08 -0300

    (página de login com bootstrap)

--- login.php
+++ login.php
@@ -40,7 +40,6 @@ if(isset($_POST["email"]) || isset($_POST["senha"])) {
         exit;
     }
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -48,21 +47,58 @@ if(isset($_POST["email"]) || isset($_POST["senha"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
 <body>
-    <h1>Login</h1>
-        <form action="" method="post">
-            <label for="email">E-mail:</label>
-            <input type="text" name="email"><br><br>
-            <label for="senha">Senha:</label>
-            <input type="password" name="senha"><br><br>
-            <button type="submit">Entrar</button>
+<div class="container d-flex justify-content-center align-items-center min-vh-100">
 
-        </form>
-    <p>Ainda não possui uma conta?</p>
-    <a href="cadastro_user.php">Cadastre-se!</a>
+<!----------------------- Login Container -------------------------->
 
+<div class="row border rounded-5 p-3 bg-white shadow box-area">
+
+<!--------------------------- Left Box ----------------------------->
+
+<div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #103cbe;">
+    <div class="featured-image mb-3">
+        <img src="gatinho-removebg-preview.png" class="img-fluid" style="width: 250px;">
+    </div>
+    <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Venha estudar conosco</p>
+    <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Cadastro de alunos com eficiência.</small>
+</div> 
+
+<!-------------------- ------ Right Box ---------------------------->
     
+<div class="col-md-6 right-box">
+    <div class="row align-items-center">
+            <div class="header-text mb-4">
+                <h2>Olá, User!</h2>
+                <p>É bom te ver Novamente.</p>
+            </div>
+            <form action="" method="post">
+                <div class="input-group mb-3">
+                    <input for="email" type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email">
+                </div>
+                <div class="input-group mb-1">
+                    <input for="senha" type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Senha">
+                </div>
+                <div class="input-group mb-5 d-flex justify-content-between">
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="formCheck">
+                        <label for="formCheck" class="form-check-label text-secondary"><small>Lembre de mim</small></label>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <button class="btn btn-lg btn-primary w-100 fs-6" type="submit">Entrar</button>
+                </div>
+                <div class="row">
+                    <small>Não tem uma conta? <a href="cadastro_user.php">Cadastre-se!</a></small>
+                </div>
+            </form>
+    </div>
+</div> 
+</div>
+
 </body>
 </html>
