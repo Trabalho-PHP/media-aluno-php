c08751d2d4993c8b7281dc3e26f2b886a4f548f5
Camilly <Camilly.p.souza@gmail.com>
2023-11-24 16:52:47 -0300

    (login definitivo)

--- login.php
+++ login.php
@@ -40,6 +40,7 @@ if(isset($_POST["email"]) || isset($_POST["senha"])) {
         exit;
     }
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -52,53 +53,54 @@ if(isset($_POST["email"]) || isset($_POST["senha"])) {
     <title>Login</title>
 </head>
 <body>
-<div class="container d-flex justify-content-center align-items-center min-vh-100">
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
 
-<!----------------------- Login Container -------------------------->
+    <!-----login container----->
 
-<div class="row border rounded-5 p-3 bg-white shadow box-area">
+    <div class="row border rounded-5 p-3 bg-white shadow box-area">
 
-<!--------------------------- Left Box ----------------------------->
+    <!-----left box----->
 
-<div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #103cbe;">
-    <div class="featured-image mb-3">
-        <img src="gatinho-removebg-preview.png" class="img-fluid" style="width: 250px;">
+    <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #103cbe;">
+        <div class="featured-image mb-3">
+            <img src="gatinho-removebg-preview.png" class="img-fluid" style="width: 250px;">
+        </div>
+        <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Venha estudar conosco!</p>
+        <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Cadastro de alunos com eficiência.</small>
     </div>
-    <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Venha estudar conosco</p>
-    <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Cadastro de alunos com eficiência.</small>
-</div> 
 
-<!-------------------- ------ Right Box ---------------------------->
-    
-<div class="col-md-6 right-box">
-    <div class="row align-items-center">
-            <div class="header-text mb-4">
-                <h2>Olá, User!</h2>
-                <p>É bom te ver Novamente.</p>
+    <!-----right box----->
+
+    <div class="col-md-6 right-box">
+        <div class="row aign-items-center">
+        <div class="header-text mb-4">
+        <h2>Olá, User!</h2>
+        <p>É bom te ver novamente.</p>
+    </div>
+    <form action="" method="post">
+        <div class="input-group mb-3">
+            <input type="text" name="email" class="form-control form-control-lg bg-light fs-6" placeholder="Email">
+        </div>
+        <div class="input-group mb-1">
+            <input type="password" name="senha" class="form-control form-control-lg bg-light fs-6" placeholder="Senha">
+        </div>
+        <div class="input-group mb-5 d-flex jutify-content- between">
+            <div class="from-check">
+                <input type="checkbox" class="form-check-input" id="formCheck">
+                <label for="formCheck" class="form-check-label text-secondary"><small>Lembre de mim</small></label>
             </div>
-            <form action="" method="post">
-                <div class="input-group mb-3">
-                    <input for="email" type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email">
-                </div>
-                <div class="input-group mb-1">
-                    <input for="senha" type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Senha">
-                </div>
-                <div class="input-group mb-5 d-flex justify-content-between">
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="formCheck">
-                        <label for="formCheck" class="form-check-label text-secondary"><small>Lembre de mim</small></label>
-                    </div>
-                </div>
-                <div class="input-group mb-3">
-                    <button class="btn btn-lg btn-primary w-100 fs-6" type="submit">Entrar</button>
-                </div>
-                <div class="row">
-                    <small>Não tem uma conta? <a href="cadastro_user.php">Cadastre-se!</a></small>
-                </div>
-            </form>
+        </div>
+        <div class="input-group mb-3">
+            <button class="btn btn-lg btn-primary w-100 fs-6" type="submit">Entrar</button>
+        </div>
+        <div class="row">
+            <small>Não tem uma conta? <a href="cadastro_user.php">Cadastre-se!</a></small>
+        </div>
+    </form>
     </div>
-</div> 
-</div>
+    </div>
+    </div>
 
+    
 </body>
 </html>
